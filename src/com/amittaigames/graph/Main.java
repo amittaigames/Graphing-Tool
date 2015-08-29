692b9556a54a61b77d1495f7bfbc038c60585f86
Gandalf1209 (Jonah) <criminal.jba@gmail.com>
2015-08-29 09:53:40 -0500

    Commit #0001
    Shows window

---
+++ src/com/amittaigames/graph/Main.java
@@ -0,0 +1,9 @@
+package com.amittaigames.graph;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Window.init("Graphing is Cool", 600, 600, 60);
+	}
+	
+}
---
+++ src/com/amittaigames/graph/Window.java
@@ -0,0 +1,79 @@
+package com.amittaigames.graph;
+
+import org.lwjgl.Sys;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+
+public class Window {
+
+	private static int width;
+	private static int height;
+	private static int fps;
+	private static long lastFrame;
+	
+	private static long lastFPS;
+	private static int LAST = 0;
+	private static int FPS;
+	
+	public static void init(String title, int width, int height, int fps) {
+		try {
+			Display.setDisplayMode(new DisplayMode(width, height));
+			Display.setTitle(title);
+			Display.setVSyncEnabled(true);
+			Display.create();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Window.width = width;
+		Window.height = height;
+		Window.fps = fps;
+		start();
+	}
+	
+	private static void start() {
+		initGL();
+		getDelta();
+		
+		while (!Display.isCloseRequested()) {
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+			
+			Display.update();
+			Display.sync(fps);
+		}
+		
+		Display.destroy();
+		System.exit(0);
+	}
+	
+	private static void initGL() {
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, width, 0, height, -1, 1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+	}
+	
+		private static int getDelta() {
+			long time = getTime();
+			int delta = (int) (time - lastFrame);
+			lastFrame = time;
+			return delta;
+		}
+		
+		private static long getTime() {
+			return (Sys.getTime() * 1000) / Sys.getTimerResolution();
+		}
+		
+		public static int getCurrentFPS() {
+			int ret = LAST;
+			if (getTime() - lastFPS > 1000) {
+				ret = FPS;
+				LAST = FPS;
+				FPS = 0;
+				lastFPS += 1000;
+			}
+			FPS++;
+			return ret;
+		}
+	
+}
